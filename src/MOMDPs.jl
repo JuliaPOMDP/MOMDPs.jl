707cc8693a2a2a04af28dc2a1da5ddbb674c96bc
Maxim Egorov <maxgegorov@gmail.com>
2015-07-14 08:23:07 -0700

    Initial commit

--- src/MOMDPs.jl
+++ src/MOMDPs.jl
@@ -1,5 +1,22 @@
 module MOMDPs
 
-# package code goes here
+include("reexport.jl")
+@reexport using POMDPs
+
+export 
+    MOMDP,
+    create_fully_obs_transition,
+    create_partially_obs_transition,
+    fully_obs_space,
+    part_obs_space
+
+
+abstract MOMDP <: POMDP
+
+create_fully_obs_transition(pomdp::MOMDP) = error("$(typeof(pomdp)) does not implement create_fully_obs_transition") 
+create_partially_obs_transition(pomdp::MOMDP) = error("$(typeof(pomdp)) does not implement create_part_obs_transition")   
+
+fully_obs_space(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement fully_obs_space")   
+part_obs_space(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement part_obs_space")   
 
 end # module
